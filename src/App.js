caa06cd8f1d4a4a03f907daecb48bbc4ede4efa1
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-09-07 20:47:27 -0400

    added dropdown on searchbar

--- src/App.js
+++ src/App.js
@@ -4,18 +4,25 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-    <div className='header bg-dark'>
-      <div className='row'>
-        <div className='brand my-1'>
-          <h1> eStore </h1>
-        </div>
-        <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
-          <input className='form-control ' placeholder='Search...'/>
-          <button> <i className='fa fa-search'/> </button>
+      <div className='header bg-dark'>
+        <div className='row'>
+          <div className='brand my-1'>
+            <h1> eStore </h1>
+          </div>
+          <div className='inp-container p-0 my-4 w-50 h-25 bg-white'>
+            <div className='dropdown m-0 p-0'>
+              <select className='select-btn w-100 p-0 m-0'>
+                <option> Men </option>
+                <option> Women </option>
+                <option> Kids </option>
+              </select>
+            </div>
+            <input className='form-control ' placeholder='Search...'/>
+            <button> <i className='fa fa-search'/> </button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
