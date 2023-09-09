4894e0311a32c4de9319c75d2e76559d63013ed9
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-09-09 15:39:59 -0400

    Map some Temp Data to productCard

--- src/Components/Products/index.js
+++ src/Components/Products/index.js
@@ -1,22 +1,58 @@
-import './_products.scss'
+import "./_products.scss";
 
+const Products = () => {
+  const productData = [
+    {
+      pName: "Jacket",
+      price: 45,
+      img: "shop-1.jpg",
+    },
+    {
+      pName: "Purse",
+      price: 45,
+      img: "shop-2.jpg",
+    },
+    {
+      pName: "Dress",
+      price: 45,
+      img: "shop-3.jpg",
+    },
+    {
+      pName: "Jeans",
+      price: 415,
+      img: "shop-4.jpg",
+    },
+    {
+      pName: "Boots",
+      price: 35,
+      img: "shop-5.jpg",
+    },
+    {
+      pName: "Bag",
+      price: 475,
+      img: "shop-6.jpg",
+    },
+  ];
 
-const Products = ()=>{
-    return(
-        <div>
-            <div className='mx-5 p-3 col-lg-3 col-md-6 product-card'>
-                <div className='product-image-container'>
-                    <img src={require('../../assets/images/shop/shop-1.jpg')} />
-                </div>
-                <div className='product-info'>
-                    <h5>
-                        <a href='#'>jacket</a>
-                    </h5>
-                    <p className='product-price'>$45</p>
-                </div>
+  return (
+    <div>
+      {productData.map((product, key) => {
+        return (
+          <div className="mx-5 p-3 col-lg-3 col-md-6 product-card">
+            <div className="product-image-container">
+              <img src={require("../../assets/images/shop/"+product.img)} />
             </div>
-        </div>
-    )
-}
+            <div className="product-info">
+              <h5>
+                <a href="#">{product.pName}</a>
+              </h5>
+              <p className="product-price">${product.price}</p>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-export default Products
+export default Products;
