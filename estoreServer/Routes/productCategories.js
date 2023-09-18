bab8de1c60cc349d5c3a8d583a1abcdaa4cda056
BrahimIsGreat Brahim coulibaly <brahimjobs@gmail.com>
2023-09-18 17:50:31 -0400

    Created productCategories route

---
+++ estoreServer/Routes/productCategories.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const productCategories = express();
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "1223",
+    database: "estore",
+    port: 3306,
+    multipleStatements: true
+});
+
+productCategories.get('/',(req,res)=>{
+    let categorydata;
+
+    pool.getConnection((err,connection)=>{
+        if(err){
+            res.status(500).send(err);
+        }else{
+            pool.query("select * from categories", (error, categories)=>{
+                if(error){
+                    categorydata = error;
+                    res.status(500).send(categorydata);
+                }else{
+                    categorydata = categories;
+                    res.status(200).send(categorydata)
+                }
+            })
+        }
+    })
+})
+
+module.exports = productCategories
--- estoreServer/index.js
+++ estoreServer/index.js
@@ -1,39 +1,10 @@
-const express = require('express')
+const express = require('express');
+const productCategories = require('./Routes/productCategories');
 const app = express();
-const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "1223",
-    database: "estore",
-    port: 3306,
-    multipleStatements: true
-});
-
-app.get('/',(req,res)=>{
-    let categorydata;
-
-    pool.getConnection((err,connection)=>{
-        if(err){
-            res.status(500).send(err);
-        }else{
-            pool.query("select * from categories", (error, categories)=>{
-                if(error){
-                    categorydata = error;
-                    res.status(500).send(categorydata);
-                }else{
-                    categorydata = categories;
-                    res.status(200).send(categorydata)
-                }
-            })
-        }
-    })
-})
-
+app.use('/productCategories',productCategories)
 
 const PORT = 5001;
-
 const server = app.listen(PORT , ()=>{
     console.log('app is running on port 5001')
 })
